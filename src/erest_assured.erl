f86cf98f1fc318a97d032f9b7795f46961332d1e
Karam Kim <zntuszntus@twinny.co.kr>
2017-03-14 00:06:06 +0900

    add test functions

--- src/erest_assured.erl
+++ src/erest_assured.erl
@@ -37,11 +37,13 @@
 ]).
 
 -export([
-  body/1
+  body/1, json/1
 ]).
 
 -export([
-  equal_to/1
+  equal_to/1, equal_to/2, is_json/0,
+  should_be_string/1, should_be_integer/1, should_be_float/1, should_be_number/1,
+  has_key/1
 ]).
 
 start() ->
@@ -138,9 +140,71 @@ equal_to(Expected) ->
     assert_it(Describe, "should be equal with expected value", Expected, Value, Expected =:= Value)
   end.
 
+-spec is_json() -> tester().
+is_json() ->
+  fun(Describe, Value) ->
+    Result =
+      try jsx:decode(Value) of
+        _ -> true
+      catch
+        error:badarg -> false
+      end,
+    assert_it(Describe, "should be JSON", Value, Result)
+  end.
+
+-spec json(tester()) -> assert().
+json(Tester) ->
+  fun(Describe, Response) ->
+    Tester(Describe, jsx:decode(erest_response:body(Response)))
+  end.
+
+-spec equal_to(string(), term()) -> tester().
+equal_to(Key, Expected) ->
+  fun(Describe, JSON) ->
+    Value = get_value_by_json_path(JSON, Key),
+    assert_it(Describe, Key ++ " should be equal with expected value", Expected, Value, Expected =:= Value)
+  end.
+
+-spec should_be_string(string()) -> tester().
+should_be_string(Key) ->
+  fun(Describe, JSON) ->
+    Value = get_value_by_json_path(JSON, Key),
+    assert_it(Describe, Key ++ " should be string", Value, is_bitstring(Value))
+  end.
+
+-spec should_be_integer(string()) -> tester().
+should_be_integer(Key) ->
+  fun(Describe, JSON) ->
+    Value = get_value_by_json_path(JSON, Key),
+    assert_it(Describe, Key ++ " should be integer", Value, is_integer(Value))
+  end.
+
+-spec should_be_float(string()) -> tester().
+should_be_float(Key) ->
+  fun(Describe, JSON) ->
+    Value = get_value_by_json_path(JSON, Key),
+    assert_it(Describe, Key ++ " should be float", Value, is_float(Value))
+  end.
+
+-spec should_be_number(string()) -> tester().
+should_be_number(Key) ->
+  fun(Describe, JSON) ->
+    Value = get_value_by_json_path(JSON, Key),
+    assert_it(Describe, Key ++ " should be number", Value, is_integer(Value) orelse is_float(Value))
+  end.
+
+-spec has_key(string()) -> tester().
+has_key(Key) ->
+  fun(Describe, JSON) ->
+    Value = get_value_by_json_path(JSON, Key),
+    assert_it(Describe, Key ++ " should be exist", Value =/= undefined)
+  end.
+
+
 %%
 %% internal functions
 %%
+
 
 gen_execute_request(Method, Path) ->
   fun(Request0) ->
@@ -149,13 +213,39 @@ gen_execute_request(Method, Path) ->
     erest_request:execute(Request)
   end.
 
+assert_it(Describe, Msg, true) -> {ok, {Describe, Msg}};
+assert_it(Describe, Msg, false) -> {fail, {Describe, Msg}}.
+assert_it(Describe, Msg, Value, true) -> {ok, {Describe, Msg, Value}};
+assert_it(Describe, Msg, Value, false) -> {fail, {Describe, Msg, Value}}.
 assert_it(Describe, Msg, Expected, Value, true) -> {ok, {Describe, Msg, Expected, Value}};
 assert_it(Describe, Msg, Expected, Value, false) -> {fail, {Describe, Msg, Expected, Value}}.
 
-print_assert({ok, {Describe, _, _, _}} = Assert) ->
+print_assert({ok, {Describe, _}} = Assert) ->
   io:format("~s success~n", [Describe]),
   Assert;
-print_assert({fail, {Describe,  Msg, Expected, Value}} = Assert) ->
+print_assert({ok, {Describe, _Msg, _}} = Assert) ->
+  print_assert({ok, {Describe, _Msg}}),
+  Assert;
+print_assert({ok, {Describe, _Msg, _Value, _}} = Assert) ->
+  print_assert({ok, {Describe, _Msg, _Value}}),
+  Assert;
+print_assert({fail, {Describe,  Msg}} = Assert) ->
   io:format("~s ~s but failed !!~n", [Describe, Msg]),
-  io:format("Expected: ~p\n\rValue: ~p~n", [Expected, Value]),
+  Assert;
+print_assert({fail, {Describe,  Msg, Value}} = Assert) ->
+  print_assert({fail, {Describe,  Msg}}),
+  io:format("Value: ~p~n", [Value]),
+  Assert;
+print_assert({fail, {Describe,  Msg, Expected, Value}} = Assert) ->
+  print_assert({fail, {Describe,  Msg}}),
+  io:format("Expected: ~p~nValue: ~p~n", [Expected, Value]),
   Assert.
+
+get_value_by_json_path(JSON, Path) ->
+  Paths = string:tokens(Path, "."),
+  get_value_by_json_path(loop, JSON, Paths).
+get_value_by_json_path(loop, undefine, _) -> undefined;
+get_value_by_json_path(loop, Value, []) -> Value;
+get_value_by_json_path(loop, JSON, [Key | T]) ->
+  NextJSON = proplists:get_value(list_to_binary(Key), JSON, undefined),
+  get_value_by_json_path(loop, NextJSON, T).
--- test/erest_assured_tests.erl
+++ test/erest_assured_tests.erl
@@ -19,6 +19,10 @@
 -define(ParamKey, test).
 -define(ParamValue, <<"value">>).
 -define(Body, <<"my erest_assured body">>).
+-define(JSONBody, <<"{\"key\": \"abcd\"}">>).
+-define(JSONInteger, <<"{\"key\": 123}">>).
+-define(JSONFloat, <<"{\"key\": 123.4}">>).
+-define(JSONNested, <<"{\"key\": {\"nested\": 123}}">>).
 -define(WrongBody, <<"my wrong erest_assured body">>).
 -define(Describe, <<"my erest_assured describe">>).
 
@@ -149,17 +153,101 @@ giver_tests(_) ->
 tester_tests(_) ->
   Response0 = erest_response:new(),
   Response  = erest_response:body(?Body, Response0),
+  ResponseJSON  = erest_response:body(?JSONBody, Response0),
+  ResponseJSONInteger  = erest_response:body(?JSONInteger, Response0),
+  ResponseJSONFloat  = erest_response:body(?JSONFloat, Response0),
+  ResponseJSONNested  = erest_response:body(?JSONNested, Response0),
 
   etest:tests([
     {?LINE, [
       {describe, "generate body equal_to right tester"},
       {run, ?_f( (erest_assured:then( [erest_assured:body(erest_assured:equal_to(?Body))] ))(?Describe, Response) )},
-      {should_be, {ok, {?Describe, "should be equal", ?Body, ?Body}}}
+      {should_be, {ok, {?Describe, "should be equal with expected value", ?Body, ?Body}}}
     ]},
     {?LINE, [
       {describe, "generate body equal_to wrong tester"},
       {run, ?_f( (erest_assured:then( [erest_assured:body(erest_assured:equal_to(?WrongBody))] ))(?Describe, Response) )},
-      {should_be, {fail, {?Describe, "should be equal", ?WrongBody, ?Body}}}
+      {should_be, {fail, {?Describe, "should be equal with expected value", ?WrongBody, ?Body}}}
+    ]},
+    {?LINE, [
+      {describe, "generate is_json tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:body(erest_assured:is_json())] ))(?Describe, ResponseJSON) )},
+      {should_be, {ok, {?Describe, "should be JSON", ?JSONBody}}}
+    ]},
+    {?LINE, [
+      {describe, "generate is_json wrong tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:body(erest_assured:is_json())] ))(?Describe, Response) )},
+      {should_be, {fail, {?Describe, "should be JSON", ?Body}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json equal_to right tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:equal_to("key", <<"abcd">>))] ))(?Describe, ResponseJSON) )},
+      {should_be, {ok, {?Describe, "key should be equal with expected value", <<"abcd">>, <<"abcd">>}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json equal_to wrong tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:equal_to("key", <<"my_value">>))] ))(?Describe, ResponseJSON) )},
+      {should_be, {fail, {?Describe, "key should be equal with expected value", <<"my_value">>, <<"abcd">>}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_string right tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_string("key"))] ))(?Describe, ResponseJSON) )},
+      {should_be, {ok, {?Describe, "key should be string", <<"abcd">>}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_string wrong tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_string("key"))] ))(?Describe, ResponseJSONInteger) )},
+      {should_be, {fail, {?Describe, "key should be string", 123}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_integer right tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_integer("key"))] ))(?Describe, ResponseJSONInteger) )},
+      {should_be, {ok, {?Describe, "key should be integer", 123}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_integer wrong tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_integer("key"))] ))(?Describe, ResponseJSONFloat) )},
+      {should_be, {fail, {?Describe, "key should be integer", 123.4}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_float right tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_float("key"))] ))(?Describe, ResponseJSONFloat) )},
+      {should_be, {ok, {?Describe, "key should be float", 123.4}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_float wrong tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_float("key"))] ))(?Describe, ResponseJSONInteger) )},
+      {should_be, {fail, {?Describe, "key should be float", 123}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_number right when integer tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_number("key"))] ))(?Describe, ResponseJSONInteger) )},
+      {should_be, {ok, {?Describe, "key should be number", 123}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_number right when float tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_number("key"))] ))(?Describe, ResponseJSONFloat) )},
+      {should_be, {ok, {?Describe, "key should be number", 123.4}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json should_be_number wrong tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:should_be_number("key"))] ))(?Describe, ResponseJSON) )},
+      {should_be, {fail, {?Describe, "key should be number", <<"abcd">>}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json has_key right tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:has_key("key"))] ))(?Describe, ResponseJSON) )},
+      {should_be, {ok, {?Describe, "key should be exist"}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json has_key wrong tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:has_key("mykey"))] ))(?Describe, ResponseJSON) )},
+      {should_be, {fail, {?Describe, "mykey should be exist"}}}
+    ]},
+    {?LINE, [
+      {describe, "generate json has_key nested key tester"},
+      {run, ?_f( (erest_assured:then( [erest_assured:json(erest_assured:has_key("key.nested"))] ))(?Describe, ResponseJSONNested) )},
+      {should_be, {ok, {?Describe, "key.nested should be exist"}}}
     ]}
   ]).
 
@@ -181,6 +269,6 @@ erest_assured_tests(_) ->
         { fun erest_request:execute/1,
           [ {parameter_should_be, [Request]},
             {with_return, Response }] }},
-      {should_be, {ok, {?Describe, "should be equal", ?Body, ?Body}}}
+      {should_be, {ok, {?Describe, "should be equal with expected value", ?Body, ?Body}}}
     ]}
   ]).
